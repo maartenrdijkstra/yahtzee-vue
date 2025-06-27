afc6aae0aa45c8b6bc6cad4410bc96a58b3eb1f1
maartenrdijkstra <maartenrdijkstra@gmail.com>
2025-06-27 15:59:39 +0200

    nog niet klaar

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <ThrownDice v-model="dice"></ThrownDice>
+  <ScoreTable v-model="dice"></ScoreTable>
+</template>
+
+<script setup>
+import ThrownDice from "./components/ThrownDice.vue";
+import ScoreTable from "./components/ScoreTable.vue";
+
+const dice = [];
+</script>
+
+<style scoped></style>
---
+++ src/components/ThrownDice.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="rollDice">Gooien!</button>
+
+  <table class="dice-table">
+    <tr v-for="(rolledDice, index) in dice" :key="index">
+      <td>{{ rolledDice }}</td>
+    </tr>
+  </table>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const dice = defineModel();
+
+const rollDice = (dice) => {
+  console.log(dice.value);
+  for (let i = 0; i < 5; i++) {
+    const thrownDice = Math.trunc(Math.random() * 6) + 1;
+    dice.value.push(thrownDice);
+  }
+};
+</script>
